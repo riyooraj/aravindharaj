55aa208de5e4f06498c0fe25b8f7bd00f92af035
riyooraj <47343815+riyooraj@users.noreply.github.com>
2019-02-05 11:27:19 +0530

    Create guvi5.py

---
+++ guvi5.py
@@ -0,0 +1,10 @@
+def main():
+ 
+
+	ch = input("Enter a character: ")
+	if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+			print( "a alphabet")
+	else:
+			print("it is not an alphabet")
+if __name__ == '__main__':
+    main()
