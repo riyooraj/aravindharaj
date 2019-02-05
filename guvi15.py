846815dd4111e6367bbc12a32525e094cabb0cff
riyooraj <47343815+riyooraj@users.noreply.github.com>
2019-02-05 11:57:54 +0530

    Create guvi15.py

---
+++ guvi15.py
@@ -0,0 +1,10 @@
+def main():
+    count = 0
+    number = int(input(""))
+    while (number > 0):
+      number = number//10
+      count = count + 1
+    print (cout)
+
+if __name__ == '__main__':
+    main()
